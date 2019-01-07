219c66c7c9ff72f442f0404bb241cb5e532d4d21
gabrieldpg <gabrielpcpg@gmail.com>
2019-01-07 16:53:03 +1300

    added classes to model and their variables

---
+++ app/src/test/java/Models/RegisteredUser.java
@@ -0,0 +1,7 @@
+package Models;
+
+public class RegisteredUser extends User {
+    private String username;
+    private String password;
+    private MealPlanner schedule;
+}
--- app/src/test/java/Models/User.java
+++ app/src/test/java/Models/User.java
@@ -1,4 +1,10 @@
 package Models;
 
+import java.util.ArrayList;
+
 public class User {
+    private ArrayList<ShoppingList> shoppingLists;
+    private ArrayList<Recipe> ownRecipes;
+    private ArrayList<Recipe> cookedRecipes;
+    private ArrayList<Recipe> favorites;
 }

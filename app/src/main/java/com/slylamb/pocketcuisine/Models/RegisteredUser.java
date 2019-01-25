fd70500f420e1c5585676f4408db1158b3a50e89
gabrieldpg <gabrielpcpg@gmail.com>
2019-01-25 20:16:31 +1300

    Finalised all methods declaration and most implementation on methods needed for recipe activity

--- app/src/main/java/com/slylamb/pocketcuisine/Models/RegisteredUser.java
+++ app/src/main/java/com/slylamb/pocketcuisine/Models/RegisteredUser.java
@@ -3,5 +3,4 @@ package com.slylamb.pocketcuisine.Models;
 public class RegisteredUser extends User {
     private String username;
     private String password;
-    private MealPlanner schedule;
 }
--- app/src/main/java/com/slylamb/pocketcuisine/Models/User.java
+++ app/src/main/java/com/slylamb/pocketcuisine/Models/User.java
@@ -21,6 +21,7 @@ public class User {
     private ArrayList<Recipe> cookedRecipes;
     private ArrayList<Recipe> favorites;
     private int theme;  // which theme did the user choose? Might change type
+    private MealPlanner planner;
     private String email;
     private String password;
     private FirebaseAuth mAuth;
@@ -139,14 +140,54 @@ public class User {
 
 
     // GABRIEL METHODS
-    /*
-    hasFavorite(recipe)
-hasCooked(recipe)
-addFavorite(recipe)
-deleteFavorite(recipe)
-addCooked(recipe)
-deleteCooked(recipe)
-addMealToMealPlanner(meal)
-addShoppingList(shoppingList)
-     */
+    // Check if recipe passed on is in this users favorites list
+   public boolean hasFavorite(Recipe recipe) {
+        int total = favorites.size();
+        // Go thru all favorites
+        for (int i = 0; i < total; i++) {
+            // If any matches the recipe passed on, return true, otherwise, false
+            if (recipe.getName().equals(favorites.get(i).getName())) {
+                return true;
+            }
+        }
+        return false;
+   }
+   // Check if recipe passed on is in the cooked list
+   public boolean hasCooked(Recipe recipe) {
+       int total = cookedRecipes.size();
+       // Go thru all favorites
+       for (int i = 0; i < total; i++) {
+           // If any matches the recipe passed on, return true, otherwise, false
+           if (recipe.getName().equals(cookedRecipes.get(i).getName())) {
+               return true;
+           }
+       }
+       return false;
+   }
+   // Add recipe to user's favorites list
+    public void addFavorite(Recipe recipe) {
+        favorites.add(recipe);
+    }
+    // Add recipe to user's cooked recipes list
+    public void addCooked(Recipe recipe) {
+        cookedRecipes.add(recipe);
+    }
+    // Delete recipe from users list of favorites
+    public void deleteFavorite(Recipe recipe) {
+        // Todo: add try and catch as there might be no recipe in favorites to remove
+        favorites.remove(recipe);
+    }
+    // Delete recipe from users list of cooked recipes
+    public void deleteCooked(Recipe recipe) {
+        // Todo: add try and catch as there might be no recipe in cooked to remove
+        cookedRecipes.remove(recipe);
+    }
+    // Add planned meal to users meal planner
+    public void addMealToMealPlanner(PlannedMeal meal) {
+        planner.addMeal(meal);
+    }
+    // Add shopping list to users shopping lists
+    public void addShoppingList(ShoppingList list) {
+        shoppingLists.add(list);
+    }
 }

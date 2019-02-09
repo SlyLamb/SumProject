6f4541155082d2b5922232c0478c9f7d17d33bf3
gabrieldpg <gabrielpcpg@gmail.com>
2019-02-09 16:08:43 +1300

    added datahandler and constants from mings branch

---
+++ app/src/main/java/com/slylamb/pocketcuisine/Util/Constants.java
@@ -0,0 +1,15 @@
+package com.slylamb.pocketcuisine.Util;
+
+public class Constants {
+    public static final int DB_VERSION = 1;
+    public static final String DB_NAME = "shoppingListDB";
+    public static final String TABLE_NAME = "plannedMealTB";
+
+    //Table columns
+    public static final String KEY_ID = "id";
+    public static final String KEY_ITEM = "item";
+    public static final String KEY_ITEM_STRING = "item_string";
+    public static final String KEY_QUANTITY = "quantity_number";
+    public static final String KEY_DATE_Time = "date_added";
+
+}

c49b4a7525535bd34fff78ecf167bb4e13501c26
Ming Luo <luoming886@gmail.com>
2019-02-11 19:57:02 +1300

    Added favourite page

---
+++ app/src/main/java/com/slylamb/pocketcuisine/Views/FavouritePageActivity.java
@@ -0,0 +1,15 @@
+package com.slylamb.pocketcuisine.Views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.slylamb.pocketcuisine.R;
+
+public class FavouritePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.favourite_page);
+    }
+}
